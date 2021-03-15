8a074da98596894d2f8fe501fb874514740248a9
Vladimir <fayst_@bk.ru>
2021-03-15 23:47:57 +0300

    Переделал UI под bootstrap, сверстал форму создания поста и реализовал лоогиру работы с формой нового поста.

--- src/app/admin/create-page/create-page.component.ts
+++ src/app/admin/create-page/create-page.component.ts
@@ -1,4 +1,7 @@
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+
+import { Post } from 'src/app/shared/interfaces';
 
 @Component({
   selector: 'app-create-page',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-page.component.scss']
 })
 export class CreatePageComponent implements OnInit {
+  form!: FormGroup
 
   constructor() { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      title: new FormControl(null, [
+        Validators.required
+      ]),
+      content: new FormControl(null, [
+        Validators.required
+      ])
+    });
   }
 
+  onSubmit = () => {
+    if (this.form.invalid) return;
+
+    const post: Post = {
+      title: this.form.value.title,
+      content: this.form.value.content,
+      date: new Date()
+    }
+
+    console.log(post);
+
+
+    this.form.reset();
+  }
 }
--- src/app/shared/interfaces.ts
+++ src/app/shared/interfaces.ts
@@ -3,3 +3,10 @@ export interface User {
   password: string
   returnSecureToken: boolean
 }
+
+export interface Post {
+  id? : string,
+  title: string,
+  content: string,
+  date: Date,
+}
