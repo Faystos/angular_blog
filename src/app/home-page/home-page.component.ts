ec3cf480c083061a19b6b55e94886c9e36b9b5c8
Vladimir Sadovnokov <fayst_@bk.ru>
2021-04-07 17:54:38 +0300

    Реализовал компонент для отображения постов на главной странице, функционал работы и лоадера

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { PostService } from './../shared/post.service';
+import { Observable } from 'rxjs';
+import { Post } from './../shared/interfaces';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent implements OnInit {  
+  posts$: Observable<Post[]>
 
-  constructor() { }
+  constructor(
+    private postService: PostService
+  ) { }
 
   ngOnInit(): void {
+    this.posts$ = this.postService.getAllPosts();
   }
 
 }
--- src/app/shared/components/post/post.component.ts
+++ src/app/shared/components/post/post.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Post } from './../../interfaces';
+import { Observable } from 'rxjs';
+import { Component, Input, OnInit } from '@angular/core';
+import { PostService } from '../../post.service';
 
 @Component({
   selector: 'app-post',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post.component.scss']
 })
 export class PostComponent implements OnInit {
+ @Input()post: Post;
+  
 
-  constructor() { }
+  constructor(
+   
+  ) { }
 
   ngOnInit(): void {
+    
   }
 
 }
