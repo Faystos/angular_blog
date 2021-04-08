5c566204f48b8d29ce029cb1588c43dd2fb2e642
Vladimir Sadovnokov <fayst_@bk.ru>
2021-04-08 11:46:24 +0300

    Реализовал функционал отображение конкретного поста на главной странице.

--- src/app/post-page/post-page.component.ts
+++ src/app/post-page/post-page.component.ts
@@ -1,4 +1,9 @@
+import { Post } from './../shared/interfaces';
+import { Observable } from 'rxjs';
+import { PostService } from './../shared/post.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-post-page',
@@ -6,10 +11,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-page.component.scss']
 })
 export class PostPageComponent implements OnInit {
+  post$: Observable<Post>;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostService
+  ) { }
 
   ngOnInit(): void {
+    this.post$ = this.route.params.pipe(
+      switchMap((params:Params) => this.postService.getPost(params['id']))
+    )
   }
 
 }
